ac5b6cf9a992c92ca422bb5ac4043ad761c0815b
calebmutisya <mutisyacalebmusau@gmail.com>
2025-05-23 13:57:09 +0300

    structured

--- components/MyDashboard.tsx
+++ components/MyDashboard.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
 export default function MyDashboard() {
+
+
+  type Statuses = {
+    num_days: number,
+    time_remaining: string,
+    date: string
+  }
+
+
+  const statuses = {
+    num_days:14,
+    time_remaining: '13:14:15',
+    date: (new Date()).toDateString()
+  }
+
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col flex-1'>
+      <div className='grid grid-cols-1 sm:grid-cols-3'>
+        {(Object.keys(statuses) as (keyof Statuses)[]).map((status) => (
+          <div key={status}>
+            <p>{status}</p>
+            <p>{statuses[status]}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
